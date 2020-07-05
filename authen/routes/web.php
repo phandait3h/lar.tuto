c00d83665888370389767c71ca146369c3876908
phandait3h <phandaiat13e@gmail.com>
2020-07-05 18:24:54 +0700

    day1

--- authen/routes/web.php
+++ authen/routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// GOM NHÓM CHO QUẢN TRỊ VIÊN
+
+Route::prefix('admin')->group(function (){
+    //trả về view dashboard
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
+
+    //view đăng ký
+    Route::get('register', 'AdminController@create')->name('admin.register');
+    //xử lý thông tin đăng kí
+    Route::post('register','AdminController@store')->name('admin.register.store');
+
+    //view đăng nhập admin
+    Route::get('login', 'Auth\Admin\LoginController@login')->name('admin.auth.login');
+    //xử lý thông tin đăng nhập
+    Route::post('login', 'Auth\Admin\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
+
+    //đăng xuất
+    Route::post('logout', 'Auth\Admin\LoginController@logout')->name('admin.auth.logout');
+
+
+
+});
